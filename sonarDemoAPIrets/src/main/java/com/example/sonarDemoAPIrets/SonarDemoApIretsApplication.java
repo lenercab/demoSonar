1a31d0e188df0f00309af93d2ba1beac8c842ed9
Leehener Cabeza <lcabeza@cobiscorp.com>
2020-03-18 15:57:34 -0500

    primer commit

---
+++ sonarDemoAPIrets/src/main/java/com/example/sonarDemoAPIrets/SonarDemoApIretsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarDemoAPIrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarDemoApIretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarDemoApIretsApplication.class, args);
+	}
+
+}
